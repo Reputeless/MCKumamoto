660b63576860a80beae9aa8adefa61ac3fd2edc2
Ryo Suzuki <reputeless+github@gmail.com>
2024-11-26 23:16:56 +0900

    update

---
+++ VisualStudio2022/Lecture-1/minicamp/Main.cpp
@@ -0,0 +1,13 @@
+﻿#include <print>
+#include "Common.hpp"
+#include "Point.hpp"
+#include "BinaryFileWriter.hpp"
+#include "BinaryFileReader.hpp"
+#include "RGB.hpp"
+#include "Image.hpp"
+#include "BMP.hpp"
+
+int main()
+{
+	std::println("Hello, minicamp!");
+}
